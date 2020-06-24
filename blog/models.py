a97c8441d927e0d7419396b91bbec31eb01a8017
Harry King <harryking@iMac.local>
2020-06-24 13:30:05 -0400

    Personal portfolio deployed

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 
+#Create blog models
+#title
+#pub date
+#body
+#image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubDate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+    
+    def pubDatePretty(self):
+        return self.pubDate.strftime("%b %e %Y")
